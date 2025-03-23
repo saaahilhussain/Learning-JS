2c8e275acb5776a90d12eab6f575d0b0c0846652
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-03-23 22:39:38 +0530

    assignment-1

---
+++ 01-fundamentals-1/Assignments/script.js
@@ -0,0 +1,6 @@
+//Solution to assignment 1
+// let [massMark, heightMark, massJohn, heightJohn] = [78, 1.69, 92, 1.95];
+// let [BMIMark, BMIJohn] = [massMark/(heightMark*heightMark), massJohn/(heightJohn*heightJohn)];
+
+// let markHigherBMI = BMIMark > BMIJohn;
+// console.log(BMIMark, BMIJohn, markHigherBMI);
