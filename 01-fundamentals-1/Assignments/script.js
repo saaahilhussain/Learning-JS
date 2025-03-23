46862a07b4e3774a7ebc519d5465f07260f95043
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-03-24 02:24:26 +0530

    assignment-2

--- 01-fundamentals-1/Assignments/script.js
+++ 01-fundamentals-1/Assignments/script.js
@@ -1,6 +1,17 @@
 //Solution to assignment 1
-// let [massMark, heightMark, massJohn, heightJohn] = [78, 1.69, 92, 1.95];
-// let [BMIMark, BMIJohn] = [massMark/(heightMark*heightMark), massJohn/(heightJohn*heightJohn)];
+ let [massMark, heightMark, massJohn, heightJohn] = [78, 1.69, 92, 1.95];
+ let [BMIMark, BMIJohn] = [massMark/(heightMark*heightMark), massJohn/(heightJohn*heightJohn)];
 
-// let markHigherBMI = BMIMark > BMIJohn;
-// console.log(BMIMark, BMIJohn, markHigherBMI);
+ let markHigherBMI = BMIMark > BMIJohn;
+ console.log(BMIMark, BMIJohn, markHigherBMI);
+
+
+//solution to assignment 2
+if(BMIMark>BMIJohn){
+    console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
+} else {
+    console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);
+}
+
+/* Write your code below. Good luck! 🙂 */
+
