af034c2f413253b5a203a271f67d41cc1f738645
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-03-29 10:26:13 +0530

    show-modal functions added

--- 04-modal-window/script.js
+++ 04-modal-window/script.js
@@ -1,10 +1,23 @@
 'use strict';
 
+//selected buttons & stored in a variable
+const btnsShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+//modal window and screen overlay stored in a variable
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
-for(let i=0; i<btnsOpenModal.length; i++)
-    console.log(btnsOpenModal[i].textContent);
+// console.log(btnsShowModal);
+
+//testing buttons
+for(let i=0; i<btnsShowModal.length; i++)
+    btnsShowModal[i].addEventListener('click', function(){
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+});
+    btnCloseModal.addEventListener('click', function(){
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    });
+
