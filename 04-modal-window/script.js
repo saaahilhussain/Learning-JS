eee19248b99f4e15920be7a1c5c901f517b5c2ac
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-03-29 10:35:35 +0530

    optimised-repeating-lines

--- 04-modal-window/script.js
+++ 04-modal-window/script.js
@@ -8,16 +8,20 @@ const btnCloseModal = document.querySelector('.close-modal');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 
-// console.log(btnsShowModal);
+//storing hide & show rules in a function order to avoid repetition of similar code blocks
 
+function showModal(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+function hideModal(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
 //testing buttons
 for(let i=0; i<btnsShowModal.length; i++)
-    btnsShowModal[i].addEventListener('click', function(){
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-});
-    btnCloseModal.addEventListener('click', function(){
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-    });
+    btnsShowModal[i].addEventListener('click', showModal);
+    btnCloseModal.addEventListener('click', hideModal);
+    overlay.addEventListener('click', hideModal);
+    
 
