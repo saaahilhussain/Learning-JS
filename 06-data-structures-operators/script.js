56aa2690b46faf0b17f6850b1bdfe80532e1f9b5
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-04-13 16:57:14 +0530

    destructuring objects-1

--- 06-data-structures-operators/script.js
+++ 06-data-structures-operators/script.js
@@ -2,20 +2,20 @@
 
 //Destructuring Arrays
 //Ex 1.1
-const books = ['The Silk Roads', 'Marcus Aurelius', 'Influence'];
+//const books = ['The Silk Roads', 'Zero to One', 'Influence'];
 
-//destructuring them into two variables
-const [firstBook, secondBook] = books;
-console.log(firstBook);
-console.log(secondBook);
+// //destructuring them into two variables
+// const [firstBook, secondBook] = books;
+// console.log(firstBook);
+// console.log(secondBook);
 
-//Ex 1.2
-//Destructure the books array into a variable called thirdBook. You must skip the first two books.
+// //Ex 1.2
+// Destructure the books array into a variable called thirdBook. You must skip the first two books.
 
-const [, , thirdBook] = books;
-console.log(thirdBook);
+// const [, , thirdBook] = books;
+// console.log(thirdBook);
 
-//Ex 1.3
+// //Ex 1.3
 //Below is the nested ratings array that contains two other arrays. Destructure the nested ratings arrays into two variables called rating and ratingsCount. In the result of your destructuring, the ratings variable should store a number 4.19, and the ratingsCount variable should store a number 144584.
 
 // const ratings = [
@@ -33,3 +33,50 @@ console.log(thirdBook);
 // const [fiveStarRatings, oneStarRatings, threeStarRatings=0] = ratingStars;
 
 // console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
+
+//Destructuring Objects
+
+const books = [
+  {
+    title: 'The Silk Roads',
+    author: 'Peter Frankopan',
+    ISBN: 6849,
+    keywords: 'History, Medieval History, Trade History',
+  },
+  {
+    title: 'Zero to One',
+    author: 'Peter Thiel',
+    ISBN: 40350,
+    keywords: 'Entrepreneurship, technology',
+  },
+  {
+    title: 'Influence',
+    author: 'Robert B. Cialdini',
+    ISBN: 92492,
+    keywords: 'Business, psychology, Persuasion',
+  },
+];
+
+//Ex 2.1
+//Destructure the first book object from the books array into variables called title, author and ISBN.
+
+const { title, author, ISBN } = books[0];
+
+//Ex 2.2
+// Each book object has the keywords property. Destructure the first book object from the books array into a variable called tags. The tags variable should be assigned with the value of the keywords property.
+
+const { keywords: tags } = books[0];
+console.log(tags);
+
+//Ex 2.3
+// The seventh book from the books array is missing the programmingLanguage property. Destructure the seventh book object (books[6]) into variables called language and programmingLanguage. Assign the programmingLanguage variable with a default value of 'unknown'.
+
+books[6] = 'The Almanack of Naval Ravikant';
+const { language, programmingLanguage = 'Unknown' } = books[6];
+console.log(programmingLanguage);
+
+//Ex 2.4
+// Below are two variables called bookTitle and bookAuthor. Reassign them with the values of the title and author properties of the first book object from the books array.
+
+let bookTitle = 'unknown';
+let bookAuthor = 'unknown';
