650533e8f5d55389e036f7b7ee26a58bb9b414d2
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-04-13 16:24:15 +0530

    destructuring arrays-1

--- 06-data-structures-operators/script.js
+++ 06-data-structures-operators/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+//Destructuring Arrays
+//Ex 1.1
+const books = ['The Silk Roads', 'Marcus Aurelius', 'Influence'];
+
+//destructuring them into two variables
+const [firstBook, secondBook] = books;
+console.log(firstBook);
+console.log(secondBook);
+
+//Ex 1.2
+//Destructure the books array into a variable called thirdBook. You must skip the first two books.
+
+const [, , thirdBook] = books;
+console.log(thirdBook);
+
+//Ex 1.3
+//Below is the nested ratings array that contains two other arrays. Destructure the nested ratings arrays into two variables called rating and ratingsCount. In the result of your destructuring, the ratings variable should store a number 4.19, and the ratingsCount variable should store a number 144584.
+
