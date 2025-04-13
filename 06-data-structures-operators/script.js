1730c1d7637a8478228fecb6da3911790eadc22d
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-04-13 16:32:11 +0530

    destructuring arrays done

--- 06-data-structures-operators/script.js
+++ 06-data-structures-operators/script.js
@@ -18,3 +18,18 @@ console.log(thirdBook);
 //Ex 1.3
 //Below is the nested ratings array that contains two other arrays. Destructure the nested ratings arrays into two variables called rating and ratingsCount. In the result of your destructuring, the ratings variable should store a number 4.19, and the ratingsCount variable should store a number 144584.
 
+// const ratings = [
+//   ['rating', 4.19],
+//   ['ratingsCount', 144584],
+// ];
+
+// const [[, rating], [, ratingCount]] = ratings;
+// console.log(rating, ratingCount);
+
+// //Ex 1.4
+// //Below is the ratingStars array. Destructure it into three variables called fiveStarRatings, oneStarRatings and threeStarRatings. Assign the threeStarRatings variable with a default value of 0.
+
+// const ratingStars = [63405, 1808];
+// const [fiveStarRatings, oneStarRatings, threeStarRatings=0] = ratingStars;
+
+// console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
