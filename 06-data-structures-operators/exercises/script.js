20b63caea408d744686f3f0cfdae4d479681a10a
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-04-14 15:27:42 +0530

    added files for exercise folder

---
+++ 06-data-structures-operators/exercises/script.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const books = [
+  {
+    title: 'The Silk Roads',
+    author: 'Peter Frankopan',
+    ISBN: 6849,
+    keywords: ['History', 'Medieval History', 'Trade History'],
+    thirdParty: {
+      goodreads: {
+        rating: 4.41, // Added from exercise page
+        ratingsCount: 1733,
+        reviewsCount: 63,
+        fiveStarRatingCount: 976,
+        oneStarRatingCount: 13,
+      },
+    },
+  },
+  {
+    title: 'Zero to One',
+    author: 'Peter Thiel',
+    ISBN: 40350,
+    keywords: 'Entrepreneurship, technology',
+    publisher: 'SH Publications',
+    thirdParty: {
+      goodreads: {
+        rating: 4.41, // <-- HERE
+        ratingsCount: 1733,
+        reviewsCount: 63,
+        fiveStarRatingCount: 976,
+        oneStarRatingCount: 13,
+      },
+    },
+  },
+  {
+    title: 'Influence',
+    author: 'Robert B. Cialdini',
+    ISBN: 92492,
+    keywords: 'Business, psychology, Persuasion',
+    thirdParty: {
+      goodreads: {
+        rating: 4.41, // <-- HERE
+        ratingsCount: 1733,
+        reviewsCount: 63,
+        fiveStarRatingCount: 976,
+        oneStarRatingCount: 13,
+      },
+    },
+  },
+];
+
+// Ex 5.1
+// Some of the book objects have the programmingLanguage property, which specifies what programming language is used in the book, for example
+
+// {
+//   title: 'Algorithms',
+//   author: ['Robert Sedgewick', 'Kevin Wayne'],
+//   ...
+//   programmingLanguage: 'Java',     // <-- HERE
+// }
+// Write a function called hasExamplesInJava that takes a book object from the books array as an argument. This function should return true if the book uses Java, or a string 'no data available' if it uses other language or no programming language at all.
+
+// Use short-circuiting.
+
+console.log(books);
+
+books[0].programmingLanguage = 'Java';
+books[1].programmingLanguage = 'JavaScript';
+console.log(books[0]);
+
+function hasExamplesInJava(book) {
+  return book.programmingLanguage === 'Java' || 'no data available';
+}
+
+console.log(hasExamplesInJava(books[0]));
+console.log(hasExamplesInJava(books[1]));
+
+//Ex 5.2
+// Some of the book objects have the onlineContent property, which is either true or false. Loop over the books array, and for the books that provide online content, log to the console a string in this format: "${title}" provides online content. Use short-circuiting.
+
+for (let i = 0; i < books.length; i++) {
+  books[i].onlineContent &&
+    console.log(`"${books[i].title}" provides online content`);
+}
