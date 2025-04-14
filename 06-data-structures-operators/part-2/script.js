622799bcf8960c4fd9ffad7ab3fe655d215b82b7
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-04-14 12:55:52 +0530

    for.. of

--- 06-data-structures-operators/part-2/script.js
+++ 06-data-structures-operators/part-2/script.js
@@ -238,7 +238,7 @@ const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 for (const item of menu.entries()) {
   console.log(`${item[0] + 1}: ${item[1]}`);
 }
-console.log('---another way---')``;
+console.log('---another way---');
 //Another way:-
 for (const [i, el] of menu.entries()) {
   console.log(`${i + 1}: ${el}`);
