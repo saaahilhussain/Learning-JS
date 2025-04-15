ce06c84e02d8f8474b96275a7bda61f6d07f1a0c
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-04-15 19:05:59 +0530

    strings-1

--- 06-data-structures-operators/part-4/script.js
+++ 06-data-structures-operators/part-4/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+function checkMiddleSeat(seat) {
+  //B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') {
+    console.log("You've got the middle seat :(");
+  } else console.log("You've got the side seat :)");
+}
+checkMiddleSeat('11B');
+checkMiddleSeat('45A');
+checkMiddleSeat('63E');
