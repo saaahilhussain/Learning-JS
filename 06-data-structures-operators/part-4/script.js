1ef4e817b689c73bc86faf4f9e1a761c7df8a337
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-04-15 21:17:18 +0530

    strings-2

--- 06-data-structures-operators/part-4/script.js
+++ 06-data-structures-operators/part-4/script.js
@@ -1,15 +1,79 @@
 'use strict';
 
 const airline = 'TAP Air Portugal';
-const plane = 'A320';
 
-function checkMiddleSeat(seat) {
-  //B and E are middle seats
-  const s = seat.slice(-1);
-  if (s === 'B' || s === 'E') {
-    console.log("You've got the middle seat :(");
-  } else console.log("You've got the side seat :)");
+console.log(airline.toUpperCase());
+console.log(airline.toLowerCase());
+
+//Fix capitalisation in name
+const passanger = 'sAhIl';
+const passangerLower = passanger.toLowerCase();
+const passangerCorrect =
+  passangerLower[0].toUpperCase() + passangerLower.slice(1);
+console.log(passangerCorrect);
+
+//Fix email
+const email = 'sh@outlook.in';
+console.log('original email: ', email);
+const loginEmail = '  sh@outLook.In   ';
+console.log('input email: ', loginEmail);
+console.log(email === loginEmail);
+
+//fix
+// const emailLower = loginEmail.toLowerCase();
+// const emailCorrect = emailLower.trim();
+// console.log(emailCorrect);
+// console.table(email === emailCorrect);
+//we can do it in a single line itself!
+
+const emailFixed = loginEmail.toLowerCase().trim();
+console.log(emailFixed);
+console.log(email === emailFixed);
+
+//the "replace" method
+
+// const priceINR = '85 Rs';
+// const priceUSD = priceINR.replace('Rs', '$'). replace(" ", '');
+// console.log(priceUSD);
+
+const announcement =
+  'All passangers come through the boarding door 45. Boarding door 45 shall close soon...';
+console.log(announcement);
+const announcementFixed = announcement.replace('door', 'gate');
+console.log(announcementFixed); //Did not work :(
+
+const announcementAll = announcement.replaceAll('door', 'gate');
+console.log(announcementAll);
+
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Airb'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the new Airbus family');
 }
-checkMiddleSeat('11B');
-checkMiddleSeat('45A');
-checkMiddleSeat('63E');
+
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed to board');
+  } else console.log('You are welcome aboard :)');
+};
+
+checkBaggage('I have a phone, a gun and chips');
+checkBaggage('I have a phone, a gun and knife for protection');
+checkBaggage('I have a laptop');
+
+// const plane = 'A320';
+
+// function checkMiddleSeat(seat) {
+//   //B and E are middle seats
+//   const s = seat.slice(-1);
+//   if (s === 'B' || s === 'E') {
+//     console.log("You've got the middle seat :(");
+//   } else console.log("You've got the side seat :)");
+// }
+// checkMiddleSeat('11B');
+// checkMiddleSeat('45A');
+// checkMiddleSeat('63E');
