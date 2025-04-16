1ba94e1e5db4584c7e2436dd02891b67831a9ed7
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-04-16 23:28:07 +0530

    Immediately Invoked Function Expressions (IIFEs)

--- 07-closer-look-at-functions/script.js
+++ 07-closer-look-at-functions/script.js
@@ -114,102 +114,131 @@
 
 //The call and apply methods
 
-const lufthansa = {
-  airline: 'lufthansa',
-  iataCode: 'LH',
-  bookings: [],
+// const lufthansa = {
+//   airline: 'lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
 
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum} `
-    );
-    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
-  },
-};
-const book = lufthansa.book;
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum} `
+//     );
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+//   },
+// };
+// const book = lufthansa.book;
 
-// lufthansa.book(380, 'Sahil Hussain');
-// lufthansa.book(370, 'Arup Biswas');
-// console.log(lufthansa);
+// // lufthansa.book(380, 'Sahil Hussain');
+// // lufthansa.book(370, 'Arup Biswas');
+// // console.log(lufthansa);
 
-const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
+// const eurowings = {
+//   airline: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
 
+// // console.log(eurowings);
+// // book.call(eurowings, 729, 'Aslam Siddik');
+// // book.call(eurowings, 571, 'Pankaj Boro');
+
+// // console.log(eurowings);
+
+// const swiss = {
+//   airline: 'SwissAir',
+//   iataCode: 'LX',
+//   bookings: [],
+// };
+
+// // book.call(swiss, 129, 'Bhabishnu Bordoloi');
+// // book.call(swiss, 439, 'Sareen Jannat haque');
+// // console.log(swiss);
+
+// // //Apply Method
+// // const flightData = [986, 'Sehnaj Sultana'];
+// // const flightData1 = [764, 'Reju Sultana'];
+
+// // book.apply(eurowings, flightData);
+// // book.apply(swiss, flightData1);
+
+// // console.log(eurowings);
+// // console.log(swiss);
+
+// //The Bind Method
+// const bookEW = book.bind(eurowings);
+// bookEW(911, 'Saklin Mustak');
+// bookEW(431, 'Imran Bhai');
 // console.log(eurowings);
-// book.call(eurowings, 729, 'Aslam Siddik');
-// book.call(eurowings, 571, 'Pankaj Boro');
 
-// console.log(eurowings);
+// //We can even pre-apply the parameters
 
-const swiss = {
-  airline: 'SwissAir',
-  iataCode: 'LX',
-  bookings: [],
+// const bookLX43 = book.bind(swiss, 43);
+// //so while calling we simply use the name parameter (refer to the function definition)
+// bookLX43('Kumar Kashyap');
+// bookLX43('Tinutpal Kashyap');
+
+// //important use case of bind method
+// //when we use objects with eventListeners!
+
+// lufthansa.planes = 45;
+// lufthansa.buyPlane = function () {
+//   console.log(this);
+
+//   this.planes++;
+//   console.log(this.planes);
+// };
+
+// document
+//   .querySelector('.btn')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// //partial application
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// //using bind
+// const addVAT = addTax.bind(null, 0.15); //this keyword nulled
+
+// console.log(addVAT(200));
+// //note:- order of the arguement is important
+
+// //small challenge
+// const addTaxRate = function (rate) {
+//   return function (value) {
+//     return value + value * rate;
+//   };
+// };
+
+// // console.log(addTaxRate(0.1)(200));
+// const addVAT2 = addTaxRate(0.1);
+// console.log(addVAT2(100));
+// console.log(addVAT2(200));
+
+//IIFE (Immediately Invoked Function Expression)
+
+const runOnce = function () {
+  console.log('This function will not run again');
 };
+runOnce();
 
-// book.call(swiss, 129, 'Bhabishnu Bordoloi');
-// book.call(swiss, 439, 'Sareen Jannat haque');
-// console.log(swiss);
+//IIFE :-
 
-// //Apply Method
-// const flightData = [986, 'Sehnaj Sultana'];
-// const flightData1 = [764, 'Reju Sultana'];
+(function () {
+  console.log('This will NEVER run again. (used iife)');
+  const privateData = 'This is a private variable';
+  console.log(privateData);
+})();
+//We can hide variables inside IIFEs.
 
-// book.apply(eurowings, flightData);
-// book.apply(swiss, flightData1);
+//iife in arrow functions
+(() => console.log('This will ALSO never RUN AGAIN'))();
 
-// console.log(eurowings);
-// console.log(swiss);
+{
+  const isPrivate = 45;
+  var isNotPrivate = 49;
+}
+console.log(isNotPrivate); // not hidden since we used 'var'
+console.log(isPrivate); //got hidden inside scope
 
-//The Bind Method
-const bookEW = book.bind(eurowings);
-bookEW(911, 'Saklin Mustak');
-bookEW(431, 'Imran Bhai');
-console.log(eurowings);
-
-//We can even pre-apply the parameters
-
-const bookLX43 = book.bind(swiss, 43);
-//so while calling we simply use the name parameter (refer to the function definition)
-bookLX43('Kumar Kashyap');
-bookLX43('Tinutpal Kashyap');
-
-//important use case of bind method
-//when we use objects with eventListeners!
-
-lufthansa.planes = 45;
-lufthansa.buyPlane = function () {
-  console.log(this);
-
-  this.planes++;
-  console.log(this.planes);
-};
-
-document
-  .querySelector('.btn')
-  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
-
-//partial application
-const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.1, 200));
-
-//using bind
-const addVAT = addTax.bind(null, 0.15); //this keyword nulled
-
-console.log(addVAT(200));
-//note:- order of the arguement is important
-
-//small challenge
-const addTaxRate = function (rate) {
-  return function (value) {
-    return value + value * rate;
-  };
-};
-
-// console.log(addTaxRate(0.1)(200));
-const addVAT2 = addTaxRate(0.1);
-console.log(addVAT2(100));
-console.log(addVAT2(200));
+//We can hide variables inside scopes using let & const
+//we cannot hide variables inside a scope using var keyword
