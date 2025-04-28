417e5b52c5bc6c378a7fe18950757cf7b1b4400b
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-04-28 17:48:05 +0530

    arr method: groupBy (ES-2024)

--- 08-Arrays/script.js
+++ 08-Arrays/script.js
@@ -6,28 +6,43 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, //%
   pin: 1111,
+  type: 'premium',
 };
 const account2 = {
   owner: 'Aslam Siddik',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8000, -30],
+  // movements: [5000, 3400, -150, -790, -3210, -1000, 8000, -30],
+  movements: [5000, 3400, -150, -790, -3210],
   interestRate: 1.5,
   pin: 2222,
+  type: 'basic',
 };
 const account3 = {
   owner: 'Arup Biswas',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  type: 'premium',
 };
 
 const account4 = {
   owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
+  // movements: [430, 1000, 700, 50, 90],
+  movements: [430],
   interestRate: 1,
   pin: 4444,
+  type: 'standard',
+};
+const account5 = {
+  owner: 'Sahil Hussain II',
+  // movements: [430, 1000, 700, 50, 90],
+  // movements: [430],
+  movements: [],
+  interestRate: 1,
+  pin: 4444,
+  type: 'basic',
 };
 
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2, account3, account4, account5];
 
 // // Elements
 // const labelWelcome = document.querySelector('.welcome');
@@ -281,8 +296,7 @@ const accounts = [account1, account2, account3, account4];
 //   .reduce((acc, mov) => acc + mov, 0);
 // console.log(movementsFlatMapped);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-const arr = ['Sahil', 'Aslam', 'Kumar', 'Anurag', 'Das'];
+// const arr = ['Sahil', 'Aslam', 'Kumar', 'Anurag', 'Das'];
 
 // console.log(arr.sort());
 
@@ -292,15 +306,38 @@ const arr = ['Sahil', 'Aslam', 'Kumar', 'Anurag', 'Das'];
 //   if (a > b) return 1;
 //   if (b > a) return -1;
 // });
-const movements2 = movements.sort((a, b) => a - b);
+// const movements2 = movements.sort((a, b) => a - b);
 
-console.log(movements2);
-//using .sort() mutates the original array. careful!
+// console.log(movements2);
+// //using .sort() mutates the original array. careful!
 
-//descending
-// const movements3 = movements.sort((a, b) => {
-//   if (a > b) return -1;
-//   if (b > a) return 1;
-// });
-const movements3 = movements.sort((a, b) => b - a);
-console.log(movements3);
+// //descending
+// // const movements3 = movements.sort((a, b) => {
+// //   if (a > b) return -1;
+// //   if (b > a) return 1;
+// // });
+// const movements3 = movements.sort((a, b) => b - a);
+// console.log(movements3);
+
+//groupBy (ES 2024)
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// const groupedMovements = Object.groupBy(movements, movement =>
+//   movement > 0 ? 'deposit' : 'withdrawal'
+// );
+
+// console.log(groupedMovements);
+
+const groupedByActivity = Object.groupBy(accounts, account => {
+  const movementCount = account.movements.length;
+  if (movementCount >= 8) return 'very active';
+  if (movementCount >= 4) return 'modertately active';
+  if (movementCount >= 1) return 'less active';
+  return 'inactive';
+});
+
+console.log(groupedByActivity);
+
+// const groupedByType = Object.groupBy(accounts, account => account.type);
+const groupedByType = Object.groupBy(accounts, ({ type }) => type);
+console.log(groupedByType);
