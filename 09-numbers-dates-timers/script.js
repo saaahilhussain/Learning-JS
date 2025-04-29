d412ec850280ccf7423e98b05f3ed11be2582dff
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-04-29 16:59:46 +0530

    Numeric Separators

--- 09-numbers-dates-timers/script.js
+++ 09-numbers-dates-timers/script.js
@@ -44,26 +44,44 @@
 // console.log(+(2.345).toFixed(2));
 
 //Lec 02 - The Remainder Operator
-console.log(5 % 2);
-console.log(5 / 2); // 5 = 2 * 2 + 1
+// console.log(5 % 2);
+// console.log(5 / 2); // 5 = 2 * 2 + 1
 
-console.log(8 % 3);
-console.log(8 / 3); // 8 = 2 * 3 + 2
+// console.log(8 % 3);
+// console.log(8 / 3); // 8 = 2 * 3 + 2
 
-console.log(6 % 2);
-console.log(6 / 2);
+// console.log(6 % 2);
+// console.log(6 / 2);
 
-console.log(7 % 2);
-console.log(7 / 2);
+// console.log(7 % 2);
+// console.log(7 / 2);
 
-const isEven = n => n % 2 === 0;
-console.log(isEven(8));
-console.log(isEven(23));
-console.log(isEven(514));
+// const isEven = n => n % 2 === 0;
+// console.log(isEven(8));
+// console.log(isEven(23));
+// console.log(isEven(514));
 
-const test = document.querySelector('.test');
+// const test = document.querySelector('.test');
 
-test.addEventListener('click', function () {
-  const words = test.textContent.split(' ');
-  test.textContent = words.reverse().join(' ');
-});
+// test.addEventListener('click', function () {
+//   const words = test.textContent.split(' ');
+//   test.textContent = words.reverse().join(' ');
+// });
+
+//Lec 03 - Numeric Separators
+
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number('230_000'));
+console.log(parseInt('230_000'));
