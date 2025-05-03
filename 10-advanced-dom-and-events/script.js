45adc5ecd6791e2af1b423998a7d7d429e2b2962
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-05-03 08:46:55 +0530

    manipulating styles-classes-attributes

--- 10-advanced-dom-and-events/script.js
+++ 10-advanced-dom-and-events/script.js
@@ -60,17 +60,62 @@ message.innerHTML =
   'We use cookies for improved functionality and analytics. <button class= "btn btn--close-cookies">Got it!</button>';
 // console.log(message);
 
-header.prepend(message);
+// header.prepend(message);
 // header.append(message); //prepend and append cant work both at the same time
-header.append(message.cloneNode(true)); //so, to make them appear in both, we need to clone the selection by using (cloneNode(true))
+// header.append(message.cloneNode(true)); //so, to make them appear in both, we need to clone the selection by using (cloneNode(true))
 
 //inserting before and after some element
 // header.before(message);
-// header.after(message);
+header.after(message);
 
 //deleting elements
 document
   .querySelector('.btn--close-cookies')
   .addEventListener('click', function () {
     message.remove();
+    // message.parentElement.removeChild(message);
   });
+message.style.width = '120%';
+
+/////styles
+
+message.style.backgroundColor = '#37383d';
+console.log(message.style.height);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).height);
+message.style.height = parseInt(getComputedStyle(message).height) + 30 + 'px';
+
+//on CSS variables
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
+
+// Don't use
+logo.clasName = 'jonas';
