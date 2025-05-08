482371aa292b0490b53c2efd87bf9cb6305621b9
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-05-08 14:04:19 +0530

    sticky navigation done

--- 10-advanced-dom-and-events/script.js
+++ 10-advanced-dom-and-events/script.js
@@ -193,14 +193,18 @@ tabContainer.addEventListener('click', function (e) {
 // observer.observe(section1);
 
 const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
 const stickyNav = function (entries) {
   const [entry] = entries; //similary to [entry] = entries[0];
-
   console.log(entry);
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
 };
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
+  rootMargin: `-${navHeight}px`, //dynamically added the height and it must be negative
 });
 
 headerObserver.observe(header);
