58a4c74df3bf0ee2b8eaae66cb60d2193e300cdd
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-05-09 16:50:58 +0530

    prototypes

--- 11-oop/script.js
+++ 11-oop/script.js
@@ -17,10 +17,42 @@ const shakil = new Person('shakil', 2004);
 
 //calling a function using the 'new' keyword :-
 // 1. a new {empty object is created}
-// 2. function is called with the the this keyword set to the {}
+// 2. function is called with the the "this" keyword set to the {}
+//      for example, in : const sahil = new Person('sahil', 2003);
+//      here the "this" keyword is set to sahil as it calls the constructor function using the new Keyword and creates an object.
 // 3. {} is then linked to a prototype
 // 4. function automatically returns the {}
 
 console.log(sahil, kumar, shakil);
 
 console.log(sahil instanceof Person);
+
+//Prototype
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2025 - this.birthYear);
+};
+
+sahil.calcAge();
+
+//Each and every function in JS has a property called "prototype" and that includes constructor functions as well
+
+//Every object created by using a certain constructor fucntion will get access to all the methods and properties that we define on the constructor's prototype property
+
+kumar.calcAge();
+shakil.calcAge();
+
+console.log(sahil.__proto__);
+console.log(sahil.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(sahil));
+console.log(Person.prototype.isPrototypeOf(kumar));
+console.log(Person.prototype.isPrototypeOf(shakil));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+/////////////////
+Person.prototype.relation = 'Friends';
+console.log(sahil);
+console.log(sahil.hasOwnProperty('relation'));
+console.log(sahil.hasOwnProperty('firstName'));
