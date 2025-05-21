f2be968a0e412ba6dedcb9a302ca0c7eb86e3be0
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-05-21 19:06:06 +0530

    Promises in parallel

--- 13-async-await-ajax-promises/script.js
+++ 13-async-await-ajax-promises/script.js
@@ -360,8 +360,36 @@ getCountryAndNeighbourData('russia');
 //   console.log('3: Finished getting location');
 // })();
 
+// ////////////////////////////////////////
+// // Self practice
+
+// const getJSON = function (url) {
+//   return fetch(url).then(response => {
+//     // console.log(response);
+//     if (!response.ok)
+//       throw new Error(`Country not found :( ${response.status}`);
+//     return response.json();
+//   });
+// };
+
+// const get3Countries = async function (c1, c2, c3) {
+//   try {
+//     const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+//     const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+//     const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+
+//     const dataAll = [data1.capital, data2.capital, data3.capital];
+//     const capitals = dataAll.map(el => el[0]);
+//     console.log(capitals);
+//   } catch (err) {
+//     console.error(`${err.message}`);
+//   }
+// };
+
+// get3Countries('india', 'usa', 'russia');
+
 ////////////////////////////////////////
-// Self practice
+// Lec 277 - Running Promises in Parallel
 
 const getJSON = function (url) {
   return fetch(url).then(response => {
@@ -374,13 +402,12 @@ const getJSON = function (url) {
 
 const get3Countries = async function (c1, c2, c3) {
   try {
-    const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
-    const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
-    const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
-
-    const dataAll = [data1.capital, data2.capital, data3.capital];
-    const capitals = dataAll.map(el => el[0]);
-    console.log(capitals);
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital).map(d => d[0]));
   } catch (err) {
     console.error(`${err.message}`);
   }
